9fc0dc6a84d46f01aaf4227c608c233d936b4b6b
NisalPerera1 <nisaldilesh14@gmail.com>
2023-03-16 13:44:11 +0530

    Modified About me page with contact info

--- resources/views/Items/about.blade.php
+++ resources/views/Items/about.blade.php
@@ -14,8 +14,9 @@
 
 
 <img src="{{ asset('Nisal Perera.png') }}" alt="Example Image" class="image" width="850" height="340">
-<div >
-    <h1 class="center">Get In Contact</h1>
+<div class="border">
+    <div>
+    <h1 class="contact">Get In Contact</h1>
     <p1 class="paraA">Address : 136/A , Pambala, Kakkapalliya</p1>
     <p1 class="paraM">Mobile </space> : +94 76 96 50 618</p1>
 </div >
@@ -25,6 +26,14 @@
     <div><a class="btn btn-primary" href="https://github.com/NisalPerera1">GitHub</a></div><br>
     <div><a class="btn btn-primary" href="https://www.linkedin.com/in/nisal-perera-0a2515210/">LinkedIn</a></div>
 </div>
+</div>
+
+<div>
+   <h6 class ="aboutmeH">I'm a self-learning, adaptable and enthusiastic person. Student of Institute of Technology University of Moratuwa.
+                        Organized and enthusiastic Technology Student and energetic fast learner 
+                        with strong communication skills. Seeking to use my technological and leadership talents at a prestigious firm. I am motivated to upgrade and 
+                        expand my skill set through mentorship and challenging projects in fast-paced environments.
+   </h6>     
 
 @endsection
 
